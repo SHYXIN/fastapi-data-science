d19ff50c4ad6d46a96f5b5d093973ade940b10ea
Py_TaiYuan_WangXin <wangxin@zhulong.com.cn>
2024-08-13 14:14:47 +0800

    第7章 Configuring CORS and protecting against CSRF attacks

---
+++ chapter07/authentication/schemas.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, EmailStr
+
+class UserBase(BaseModel):
+    email: EmailStr
+
+    class Config:
+        orm_mode = True
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    hashed_password: str
+
+class UserRead(UserBase):
+    id: int
